f0b3dc855274310c0666faebbd48b445c90d5264
Rayhan Zulfitri Dwi Cahyo <rayhanzulfitri@gmail.com>
2024-11-01 09:20:02 +0700

    Update fix

--- lib/data/firebase_options.dart
+++ lib/data/firebase_options.dart
@@ -54,10 +54,10 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDyZKsx8NmaLEPtpkAykWAoY2oyad6rUMA',
-    appId: '1:1013223941565:android:4b7343a1bfa681e5765ed4',
+    appId: '1:1013223941565:android:000f9b24002fde92765ed4',
     messagingSenderId: '1013223941565',
     projectId: 'aplikasi-bacaan-32e65',
     databaseURL: 'https://aplikasi-bacaan-32e65-default-rtdb.firebaseio.com',
-    storageBucket: 'aplikasi-bacaan-32e65.appspot.com',
+    storageBucket: 'aplikasi-bacaan-32e65.firebasestorage.app',
   );
 }
